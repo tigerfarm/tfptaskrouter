fe8b11b21de3468f873f41bbcdb0d6d469b07ebb
Stacy David Thurston <tigerfarm@gmail.com>
2024-02-16 17:40:08 -0800

    update

---
+++ conferenceSetCompleted.js
@@ -0,0 +1,18 @@
+console.log("+++ End a conference call.");
+// https://www.twilio.com/docs/voice/api/conference-resource
+// 
+//          1         2         3         4         5
+// 123456789012345678901234567890123456789012345678901234567890
+// Thu Feb 15 2024 19:32:44 GMT-0800 (Pacific Standard Time)
+//
+const ACCOUNT_SID = process.env.TR_ACCOUNT_SID;
+const ACCOUNT_AUTH_TOKEN = process.env.TR_AUTH_TOKEN;
+const trClient = require('twilio')(ACCOUNT_SID, ACCOUNT_AUTH_TOKEN);
+
+conferenceSid = "CF20dd0670c21ddb1684dbb51b83d5ce6a";
+console.log("+ theConferenceSid: " + conferenceSid);
+trClient.conferences(conferenceSid)
+        .update({status: 'completed'})
+        .then(conference =>
+            console.log("++ Conference ended, set to completed: " + conference.friendlyName)
+        );
